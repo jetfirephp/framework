7ecdf72e0bbe3288e831ddd7d26ab4e388f6f625
Mugan27 <mugan27@gmail.com>
2016-03-22 10:03:32 +0100

    fix

--- src/Providers/RoutingProvider.php
+++ src/Providers/RoutingProvider.php
@@ -71,8 +71,8 @@ class RoutingProvider extends Provider
                 $path = (substr($block['path'], -4) == '.php') ? $block['path'] : rtrim($block['path'], '/') . '/routes.php';
                 $block['view_dir'] = isset($block['view_dir']) ? $block['view_dir'] : rtrim($block['path'], '/') . '/Views';
                 $options = isset($block['prefix'])
-                    ? ['block' => $block['path'], 'view_dir' => $block['view_dir'], 'ctrl_namespace' => $block['namespace'] . '\Controllers', 'prefix' => $block['prefix']]
-                    : ['block' => $block['path'], 'view_dir' => $block['view_dir'], 'ctrl_namespace' => $block['namespace'] . '\Controllers'];
+                    ? ['block' => $block['path'], 'view_dir' => $block['view_dir'], 'ctrl_namespace' => $block['ctrl_namespace'] . '\Controllers', 'prefix' => $block['prefix']]
+                    : ['block' => $block['path'], 'view_dir' => $block['view_dir'], 'ctrl_namespace' => $block['ctrl_namespace'] . '\Controllers'];
                 $this->collection->addRoutes($path, $options);
             }
         }
