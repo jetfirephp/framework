5afd94766f8ee01e74f0d9a2928fd922ed3c8ef0
Mugan27 <mugan27@gmail.com>
2016-12-02 22:52:34 +0100

    add Mail component in system

---
+++ src/System/Mail.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace JetFire\Framework\System;
+
+use JetFire\Framework\App;
+use JetFire\Mailer\Mail as MailComponent;
+use JetFire\Mailer\MailerInterface;
+
+/**
+ * Class Mail
+ * @package JetFire\Framework\System
+ */
+class Mail extends MailComponent
+{
+    /**
+     * @var App
+     */
+    private $app;
+
+    /**
+     * Mail constructor.
+     * @param App $app
+     * @param MailerInterface $mailer
+     */
+    public function __construct(App $app, MailerInterface $mailer)
+    {
+        parent::__construct($mailer);
+        $this->app = $app;
+    }
+
+    /**
+     * @param $to
+     * @param $subject
+     * @param $content
+     * @param null $file
+     */
+    public function sendTo($to, $subject, $content, $file = null)
+    {
+        $from = (isset($this->app->data['app']['settings']['mail']['from']))
+            ? $this->app->data['app']['settings']['mail']['from']
+            : 'contact@jetfire.fr';
+        /** @var MailerInterface $message */
+        $message = $this->mailer->getMail()->to($to)
+            ->from($from)
+            ->subject($subject)
+            ->html($content);
+        if (!is_null($file))
+            $message->file($file);
+        $message->send();
+    }
+
+} 
