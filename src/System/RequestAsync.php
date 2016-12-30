8b40d6c09beb3c4b58b6880e30018a439bffc25d
Sumi <mugan27@gmail.com>
2016-12-30 17:17:39 +0100

    update asynchronous request

--- src/System/RequestAsync.php
+++ src/System/RequestAsync.php
@@ -11,52 +11,62 @@ class RequestAsync
 {
     /**
      * @param $address
-     * @return int
+     * @param array $data
      */
-    public function get($address)
+    public function get($address, $data = [])
     {
-        $url = parse_url($address);
+        $post_params = [];
         $errno = '';
         $errstr = '';
+        foreach ($data as $key => &$val) {
+            if (is_array($val)) $val = implode(',', $val);
+            $post_params[] = $key.'='.urlencode($val);
+        }
+        $post_string = implode('&', $post_params);
 
-        $fp = fsocketopen($url['host'], 80, $errno, $errstr, 30);
+        $url = parse_url($address);
 
-        $out = "GET " . $url['path'] . " HTTP/1.1\r\n";
+        $fp = fsockopen($url['host'], 80, $errno, $errstr, 30);
+
+        $url['path'] .= '?' . $post_string;
+
+        $out = "POST " . $url['path'] . " HTTP/1.1\r\n";
         $out .= "Host: " . $url['host'] . "\r\n";
-        $out .= "Content-Type: text/json\r\n";
-        $out .= "Content-Length: 0\r\n";
-        $out .= "Connection: Close\r\n";
+        $out .= "Content-Type: application/x-www-form-urlencoded\r\n";
+        $out .= "Content-Length: " . strlen($post_string) . "\r\n";
+        $out .= "Connection: Close\r\n\r\n";
 
         fwrite($fp, $out);
         fclose($fp);
-
-        return 0;
     }
 
     /**
      * @param $address
      * @param $data
-     * @return int
      */
-    public function post($address, $data)
+    public function post($address, $data = [])
     {
-        $post_string = json_encode($data);
-        $url = parse_url($address);
+        $post_params = [];
         $errno = '';
         $errstr = '';
+        foreach ($data as $key => &$val) {
+            if (is_array($val)) $val = implode(',', $val);
+            $post_params[] = $key.'='.urlencode($val);
+        }
+        $post_string = implode('&', $post_params);
 
-        $fp = fsocketopen($url['host'], 80, $errno, $errstr, 30);
+        $url = parse_url($address);
+
+        $fp = fsockopen($url['host'], 80, $errno, $errstr, 30);
 
         $out = "POST " . $url['path'] . " HTTP/1.1\r\n";
         $out .= "Host: " . $url['host'] . "\r\n";
-        $out .= "Content-Type: text/json\r\n";
+        $out .= "Content-Type: application/x-www-form-urlencoded\r\n";
         $out .= "Content-Length: " . strlen($post_string) . "\r\n";
-        $out .= "Connection: Close\r\n";
-        $out .= $post_string;
+        $out .= "Connection: Close\r\n\r\n";
+        if (isset($post_string)) $out .= $post_string;
 
         fwrite($fp, $out);
         fclose($fp);
-
-        return 0;
     }
 }
