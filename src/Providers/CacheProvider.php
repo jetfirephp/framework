08d8363d32f8535d9e5d839747f73b2b0fc71bd3
Mugan27 <mugan27@gmail.com>
2017-02-25 14:50:22 +0100

    fix cache provider issue

--- src/Providers/CacheProvider.php
+++ src/Providers/CacheProvider.php
@@ -23,9 +23,9 @@ class CacheProvider extends Provider
      * @var array
      */
     private $cacheDrivers = [
-        'Doctrine\Common\Cache\ArrayCache' => 'getCache',
-        'Doctrine\Common\Cache\ApcCache' => 'getCache',
-        'Doctrine\Common\Cache\XcacheCache' => 'getCache',
+        'Doctrine\Common\Cache\ArrayCache' => 'getNativeCache',
+        'Doctrine\Common\Cache\ApcCache' => 'getNativeCache',
+        'Doctrine\Common\Cache\XcacheCache' => 'getNativeCache',
         'Doctrine\Common\Cache\FilesystemCache' => 'getFileCache'
     ];
 
@@ -48,19 +48,31 @@ class CacheProvider extends Provider
      */
     public function getCache($key = null)
     {
-        if (is_null($key))
+        if (is_null($key)) {
             return $this->cache;
-        elseif (isset($this->cacheDrivers[$this->config['drivers'][$key]['class']])) {
-            return call_user_func_array([$this, $this->cacheDrivers[$this->config['drivers'][$key]['class']]], [$this->config['drivers'][$key]]);
+        }elseif (isset($this->cacheDrivers[$this->config['drivers'][$key]['class']])) {
+            return $this->cache = call_user_func_array([$this, $this->cacheDrivers[$this->config['drivers'][$key]['class']]], [$this->config['drivers'][$key]]);
         } elseif (isset($this->config['drivers'][$key]['callback'])) {
             $callback = $this->config['drivers'][$key]['callback'];
             if (!$this->app->has($callback))
                 throw new \Exception('Callback : ' . $callback . ' not found in DI.');
             if (!method_exists(($provider = $this->app->get($callback)), 'getCache'))
                 throw new \Exception('Method "getCache" not found in ' . get_class($provider));
-            return $provider->getCache($this->config['drivers'][$key]);
+            return $this->cache = $provider->getCache($this->config['drivers'][$key]);
         }
         return null;
+    }
+
+    /**
+     * @param $driver
+     * @return mixed
+     */
+    private function getNativeCache($driver)
+    {
+        $this->app->addRule($driver['class'], [
+            'shared' => true
+        ]);
+        return $this->app->get($driver['class']);
     }
 
     /**
@@ -70,7 +82,7 @@ class CacheProvider extends Provider
     private function getFileCache($driver)
     {
         if (!isset($driver['args'][0]) || !isset($driver['args'][1]))
-            throw new InvalidArgumentException('Arguments for memcache driver missing');
+            throw new InvalidArgumentException('Arguments for file cache driver missing');
         $this->app->addRule($driver['class'], [
             'shared' => true,
             'construct' => [$driver['args'][0], $driver['args'][1]]
