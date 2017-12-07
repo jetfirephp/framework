983d5cf0112c4912cce12a2fe00d859013e58722
Mugan27 <mugan27@gmail.com>
2017-12-07 19:59:56 +0100

    fix routing provider

--- src/Providers/RoutingProvider.php
+++ src/Providers/RoutingProvider.php
@@ -67,14 +67,15 @@ class RoutingProvider extends Provider
     {
         foreach ($routes as $key => $block) {
             if (is_array($block)) {
-                $path = (substr($block['path'], -4) == '.php') ? $block['path'] : rtrim($block['path'], '/') . '/routes.php';
+                $route = isset($block['route']) ? $block['route'] : [];
                 $block['view_dir'] = isset($block['view_dir']) ? $block['view_dir'] : rtrim($block['path'], '/') . '/Views';
                 $options = isset($block['prefix'])
                     ? ['block' => $block['path'], 'view_dir' => $block['view_dir'], 'ctrl_namespace' => $block['namespace'] . '\Controllers', 'prefix' => $block['prefix']]
                     : ['block' => $block['path'], 'view_dir' => $block['view_dir'], 'ctrl_namespace' => $block['namespace'] . '\Controllers'];
                 if (isset($block['subdomain'])) $options['subdomain'] = $block['subdomain'];
                 if (isset($block['protocol'])) $options['protocol'] = $block['protocol'];
-                $this->collection->addRoutes($path, $options);
+                if (isset($block['params'])) $options['params'] = $block['params'];
+                $this->collection->addRoutes($route, $options);
             }
         }
     }
